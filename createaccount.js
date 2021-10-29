a141f72d6f498bde471fa32d2952c6346fea4de4
gorkhaliSujan <56081103+gorkhaliSujan@users.noreply.github.com>
2021-10-28 23:40:42 -0400

    adding password length validation and adding deposit page

--- createaccount.js
+++ createaccount.js
@@ -12,6 +12,12 @@ function CreateAccount(){
         setTimeout(() => setStatus(''),3000);
         return false;
       }
+      if(label=="password" && field.length<8)
+      {
+        setStatus(`Error ${label} must be at least 8 characters`);
+        setTimeout(() => setStatus(''),3000);
+        return false;
+      }
       return true;
   }
 
@@ -23,6 +29,7 @@ function CreateAccount(){
     ctx.users.push({name,email,password,balance:100});
     setShow(false);
   }    
+  
 
   function clearForm(){
     setName('');
@@ -44,12 +51,12 @@ function CreateAccount(){
               <input type="input" className="form-control" id="email" placeholder="Enter email" value={email} onChange={e => setEmail(e.currentTarget.value)}/><br/>
               Password<br/>
               <input type="password" className="form-control" id="password" placeholder="Enter password" value={password} onChange={e => setPassword(e.currentTarget.value)}/><br/>
-              <button type="submit" className="btn btn-light" onClick={handleCreate}>Create Account</button>
+              <button type="submit" className="btn btn-light" onClick={handleCreate} >Create Account</button>
               </>
             ):(
               <>
               <h5>Success</h5>
-              <button type="submit" className="btn btn-light" onClick={clearForm}>Add another account</button>
+               <button  type="submit" className="btn btn-light" onClick={clearForm} >Add another account</button>
               </>
             )}
     />
--- deposit.js
+++ deposit.js
@@ -1,5 +1,35 @@
 function Deposit(){
-  return (
-    <h1>Deposit</h1>
-  )
+  const [show, setShow]         = React.useState(true);
+  const [amount, setAmount]         = React.useState(0);
+  const [newBalance, setnewBalance]         = React.useState(0);
+
+  const ctx = React.useContext(UserContext);  
+
+  function handleDeposit(){
+    let newTotal= Number(amount)+ Number(ctx.users[0].balance);
+    setnewBalance(newTotal);
+    console.log(amount,ctx.users[0], (Number(amount)+ Number(ctx.users[0].balance)));
+   
+    let user= ctx.users[0];
+    user.balance=newTotal;
+    ctx.users.push(user);
+    console.log( user);
+    setShow(false);
+  }    
+
+ return (<Card
+  bgcolor="primary"
+  header="Create Account"
+  status={status}
+  body={ 
+          <>
+          BALANCE   
+          <span   id="balance"  style={{paddingLeft: "150px"}}>{ctx.users[0].balance} </span><br/>
+          DEPOSIT AMOUNT<br/>
+          <input type="input" className="form-control" id="deposit" placeholder="Enter amount" value={amount} onChange={e => setAmount(e.currentTarget.value)}/><br/>
+          <button type="submit" className="btn btn-light" onClick={handleDeposit} >Deposit</button>
+          </>
+        
+}
+/>)
 }

d933ad1323a1d2a66242206a3dd8e0f38cab3ab8
gorkhaliSujan <56081103+gorkhaliSujan@users.noreply.github.com>
2021-10-29 00:09:04 -0400

    adding withdraw functionality
    withdraw deducts from the total amount

--- withdraw.js
+++ withdraw.js
@@ -1,5 +1,48 @@
 function Withdraw(){
-  return (
-    <h1>Withdraw</h1>
-  )
-}
+ 
+  const [show, setShow]         = React.useState(true);
+  const [amount, setAmount]         = React.useState(0);
+  const [newBalance, setnewBalance]         = React.useState(0);
+
+  const ctx = React.useContext(UserContext);  
+
+  function handleDeposit(){
+    let newTotal= Number(ctx.users[0].balance)-Number(amount);
+    setnewBalance(newTotal);
+    console.log(amount,ctx.users[0], (Number(amount)+ Number(ctx.users[0].balance)));
+   
+    let user= ctx.users[0];
+    user.balance=newTotal;
+    ctx.users.push(user);
+    console.log( user);
+    setShow(false);
+  }    
+
+  function clearForm(){
+    setAmount(0);
+    setShow(true);
+  }
+
+ return (<Card
+  bgcolor="primary"
+  header="Withdraw"
+  status={status}
+  body={ show ? ( 
+          <>
+          BALANCE   
+          <span   id="balance"  style={{paddingLeft: "150px"}}>{ctx.users[0].balance} </span><br/>
+          Withdraw Amount<br/>
+          <input type="input" className="form-control" id="deposit" placeholder="Enter amount" value={amount} onChange={e => setAmount(e.currentTarget.value)}/><br/>
+          <button type="submit" className="btn btn-light" onClick={handleDeposit} >Withdraw</button>
+          </>
+  ):(
+    <>
+    <h5>Withdraw sucessful.</h5> 
+    <h5>New Balance: {newBalance}</h5>
+     <button  type="submit" className="btn btn-light" onClick={clearForm} > Withdraw another amount
+    </button>
+    </>
+             )}
+             />
+           )
+         }
